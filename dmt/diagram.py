d540bba7842180eb3d49cc26ca89cd23840d2d5d
Maxime Perrotin <maxime.perrotin@esa.int>
2022-01-19 11:11:09 +0100

    Create a separate function for the db diagram creation
    diagram.py created png diagrams for database schemas but postgres engine
    was hardcoded. Added a separate function that can be called from a
    python script (from dmt import diagram)

--- dmt/diagram.py
+++ dmt/diagram.py
@@ -5,23 +5,25 @@ import sys
 from sqlalchemy import MetaData
 from sqlalchemy_schemadisplay import create_schema_graph
 
-
-def main():
-    if len(sys.argv) < 2:
-        print("Usage: " + sys.argv[0] + ' <database_name>')
-        sys.exit(1)
-
+def create_diagram(path: str, name: str):
     # create the pydot graph object by autoloading all tables via a bound metadata object
+    # path is the link to the database such as postgresql+psycopg2://taste:tastedb@localhost/blah
     graph = create_schema_graph(
-        metadata=MetaData(
-            'postgresql+psycopg2://taste:tastedb@localhost/' + sys.argv[1]),
+        metadata=MetaData(path),
         show_datatypes=False,  # The image would get too big if we'd show the datatypes
         show_indexes=False,    # ditto for indexes
         rankdir='LR',          # From left to right (instead of top to bottom)
         concentrate=True       # Don't try to join the relation lines together
     )
-    graph.write_png(sys.argv[1] + '.png')  # write out the file
+    graph.write_png(f'{name}.png')  # write out the file
+    
 
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: " + sys.argv[0] + ' <path to database> <database_name>')
+        print(f"e.g. {sys.argv[0]} postgresql+psycopg2://taste:tastedb@localhost/blah blah")
+        sys.exit(1)
+    create_diagram(sys.argv[1], sys.argv[2])
 
 if __name__ == "__main__":
     main()
